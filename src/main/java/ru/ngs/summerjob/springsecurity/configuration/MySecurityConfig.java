c3d132c857c45a3c7b93c378191e8a8b4991d801
Sergei Kovalev <summerjob@ngs.ru>
2023-02-27 18:10:46 +0300

    view added. Security Config upgraded.

--- src/main/java/ru/ngs/summerjob/springsecurity/configuration/MySecurityConfig.java
+++ src/main/java/ru/ngs/summerjob/springsecurity/configuration/MySecurityConfig.java
@@ -1,39 +1,33 @@
 package ru.ngs.summerjob.springsecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+////        http
+////                .csrf().disable()
+////                .authorizeHttpRequests()
+////                .anyRequest().authenticated()
+////                .and()
+////                .httpBasic();
+//
+//        return http.build();
+//    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        List<UserDetails> users = new ArrayList<>();
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("sergey")
-                .password("sergey")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("elena")
-                .password("elena")
-                .roles("HR")
-                .build();
-        UserDetails user3 = User.withDefaultPasswordEncoder()
-                .username("ivan")
-                .password("ivan")
-                .roles("MANAGER", "HR")
-                .build();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        return new InMemoryUserDetailsManager(users);
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder().username("sergey").password("sergey").roles("EMPLOYEE").build(),
+                User.withDefaultPasswordEncoder().username("elena").password("elena").roles("HR").build(),
+                User.withDefaultPasswordEncoder().username("ivan").password("ivan").roles("MANAGER", "HR").build());
     }
 
 }
