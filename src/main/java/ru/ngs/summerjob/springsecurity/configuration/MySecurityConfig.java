555a8b962dec9bea1140f6f706d39a8e591deb39
Sergei Kovalev <summerjob@ngs.ru>
2023-02-28 16:25:17 +0300

    authorization by roles.

--- src/main/java/ru/ngs/summerjob/springsecurity/configuration/MySecurityConfig.java
+++ src/main/java/ru/ngs/summerjob/springsecurity/configuration/MySecurityConfig.java
@@ -2,25 +2,32 @@ package ru.ngs.summerjob.springsecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        http
-////                .csrf().disable()
-////                .authorizeHttpRequests()
-////                .anyRequest().authenticated()
-////                .and()
-////                .httpBasic();
-//
-//        return http.build();
-//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> {
+                    try {
+                        authz
+                                .requestMatchers("/").hasAnyRole("EMPLOYEE", "MANAGER", "HR")
+                                .requestMatchers("/hr_info").hasRole("HR")
+                                .requestMatchers("/manager_info/**").hasRole("MANAGER")
+                                .anyRequest().authenticated().and().formLogin().permitAll();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
